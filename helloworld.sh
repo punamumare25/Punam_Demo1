78814f7eae9d0d0f73f92f484ebf6d17367e238d
Punam <punamumare25@gmail.com>
2020-07-27 12:35:46 +0530

    gotignore demo

--- helloworld.sh
+++ helloworld.sh
@@ -5,7 +5,7 @@ public static void main(String args[])
 while(true)
 {
 =======
-System.out.println("my master branch");
+System.out.println("my working application");
 System.out.println("this is my new feature implemented in develop");
 System.out.println("this is my another feature");
 try
