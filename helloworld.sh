9cb4901f67206046996160766758f0198ebef54c
Punam <punamumare25@gmail.com>
2020-07-27 11:32:52 +0530

    master branch updation

--- helloworld.sh
+++ helloworld.sh
@@ -4,10 +4,8 @@ public static void main(String args[])
 {
 while(true)
 {
-System.out.println("Hello to terminal commands");
-System.out.println("Hello Punam");
 =======
-System.out.println("Hello Punam creating conflict");
+System.out.println("my master branch");
 try
 {
 Thread.sleep(6000);
